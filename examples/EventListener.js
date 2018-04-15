c2c32e979b5e93e608a8fbc5d48c610996120f31
Fisher <fisher.subs@gmail.com>
2018-04-15 20:32:54 +0800

    [examples/EventListener] Added an example of EventListener.

---
+++ examples/EventListener.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const EventListener = require('../EventListener');
+
+const mEventListener = new EventListener();
+
+mEventListener.addListener(() => {
+	console.log('A permanent event is triggered.');
+});
+
+mEventListener.addTempListener(() => {
+	console.log('A temp event is triggered.');
+});
+
+
+mEventListener.triggerEvent();
+
+mEventListener.addListener(() => {
+	console.log('Another temp event is triggered.');
+}, true);
+
+mEventListener.triggerEvent();
+mEventListener.triggerEvent();
