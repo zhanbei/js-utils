46ad6e7880b19aaeed8ea7261c91c9ccacbbad0b
Fisher <fisher.subs@gmail.com>
2018-07-30 17:58:57 +0800

    [HtmlScriptUtil] Added asynchronous #loadAndExecuteScript();
    - to load and execute a script in a html document asynchronously.
    - @see https://stackoverflow.com/questions/8578617/inject-a-script-tag-
    with-remote-src-and-wait-for-it-to-execute

---
+++ HtmlScriptUtil.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Load and execute a script in a html document asynchronously.
+// @see https://stackoverflow.com/questions/8578617/inject-a-script-tag-with-remote-src-and-wait-for-it-to-execute
+exports.loadAndExecuteScript = (scriptUrl, callback) => {
+	const script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.async = true;
+	script.onload = callback;
+	script.src = scriptUrl;
+	document.getElementsByTagName('head')[0].appendChild(script);
+};
