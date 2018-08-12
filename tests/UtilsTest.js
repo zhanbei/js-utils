c1a795cd3b3d4b04f31070f7dcddb871c7ba461e
Fisher <fisher.subs@gmail.com>
2018-08-12 22:59:19 +0800

    [tests/UtilsTest] Upgraded tests; added test of #IntervalTimeUtil.

--- tests/UtilsTest.js
+++ tests/UtilsTest.js
@@ -1,9 +1,28 @@
 'use strict';
 
-const ClockUtils = require('./../ClockUtils');
-const ErrorUtils = require('./../ErrorUtils');
+const ClockUtils = require('../ClockUtil');
+const ErrorUtils = require('../ErrorUtil');
+const ReadableIntervalUtil = require('../IntervalTimeUtil');
 
-ClockUtils.waitForALittleWhile(() => true, 1500, 6, () => console.log('Waiting')).then(() => {
-	console.log('Finished');
-}).catch(ex => ErrorUtils.handleError('', ex));
 
+function testClockUtil() {
+	ClockUtils.waitForALittleWhile(() => true, 1500, 6, () => console.log('Waiting')).then(() => {
+		console.log('Finished');
+	}).catch(ex => ErrorUtils.handleError('', ex));
+}
+
+function testIntervalTimeUtil() {
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(10));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(60));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(65));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(301));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(600));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(610));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(32320));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(132890));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(143920890));
+	console.log(ReadableIntervalUtil.getReadableIntervalTime(13234232890));
+}
+
+testClockUtil();
+testIntervalTimeUtil();
