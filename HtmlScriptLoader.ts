969e9b4ffc87f71e56ca19e090a9ab8ca527b1ec
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-03-19 13:16:56 +0800

    [Html(Script|Stylesheet)Loader] Upgraded funcs to reduce duplication.

--- HtmlScriptLoader.ts
+++ HtmlScriptLoader.ts
@@ -2,23 +2,16 @@
 
 // Load and execute a script in a html document asynchronously.
 // @see https://stackoverflow.com/questions/8578617/inject-a-script-tag-with-remote-src-and-wait-for-it-to-execute
-export const loadAndExecuteScript = (scriptUrl: string, callback: (...params: any[]) => any) => {
+export const loadAndExecuteScript = (scriptUrl: string, callback: (...params: any[]) => any, onError?: (...params: any[]) => any) => {
 	const script = document.createElement('script');
 	script.type = 'text/javascript';
 	script.async = true;
 	script.onload = callback;
+	if (onError) {script.onerror = onError;}
 	script.src = scriptUrl;
 	document.getElementsByTagName('head')[0].appendChild(script);
 };
 
-export const loadScript = (scriptUrl: string) => {
-	return new Promise((resolve, reject) => {
-		const script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.src = scriptUrl;
-		script.async = true;
-		script.onload = resolve;
-		script.onerror = reject;
-		document.getElementsByTagName('head')[0].appendChild(script);
-	});
-};
+export const loadScript = (scriptUrl: string) => new Promise((resolve, reject) => {
+	loadAndExecuteScript(scriptUrl, resolve, reject);
+});
--- HtmlStylesheetLoader.ts
+++ HtmlStylesheetLoader.ts
@@ -1,23 +1,16 @@
 'use strict';
 
 // @see #HtmlScriptLoader.loadAndExecuteScript();
-export const loadStyleAsynchronously = (styleUrl: string, callback: (...params: any[]) => any) => {
+export const loadStyleAsynchronously = (styleUrl: string, callback: (...params: any[]) => any, onError?: (...params: any[]) => any) => {
 	const link = document.createElement('link');
 	link.rel = 'stylesheet';
 	// link.async = true;
 	link.onload = callback;
+	if (onError) {link.onerror = onError;}
 	link.href = styleUrl;
 	document.getElementsByTagName('head')[0].appendChild(link);
 };
 
-export const loadStyle = (styleUrl: string) => {
-	return new Promise((resolve, reject) => {
-		const link = document.createElement('link');
-		link.rel = 'stylesheet';
-		// link.async = true;
-		link.onload = resolve;
-		link.onerror = reject;
-		link.href = styleUrl;
-		document.getElementsByTagName('head')[0].appendChild(link);
-	});
-};
+export const loadStyle = (styleUrl: string) => new Promise((resolve, reject) => {
+	loadStyleAsynchronously(styleUrl, resolve, reject);
+});
