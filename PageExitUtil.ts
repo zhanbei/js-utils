8c53e099466b7a8ef9d714c6c19c47786861c3f2
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-06-04 19:35:24 +0800

    [PageExitUtil] Added complicated func to remind user exiting a web page.

--- PageExitUtil.ts
+++ PageExitUtil.ts
@@ -7,7 +7,45 @@ export const setExitPageReminder = (hint?: string) =>
 
 export const enableExitPageReminder = () =>
 	window.onbeforeunload = () => mHint;
-enableExitPageReminder();
 
 export const disableExitPageReminder = () =>
 	window.onbeforeunload = null;
+
+const enablePageCloseReminder = (hint?: string) => {
+	setExitPageReminder(hint);
+	enableExitPageReminder();
+};
+
+// As for kinds of browsers do not support custom alert on leave site, and even some do not support alert on exit.
+// We use a alternative way(routes listening) to achieve the goal to remind users about something important.
+const enablePageBackReminder = (
+	hint?: string, title: string = document.title, url: string = '#' + Math.random(),
+) => {
+	const pushHistoryPage = () => {
+		const state = {title, url};
+		window.history.pushState(state, title, url);
+	};
+
+	pushHistoryPage();
+
+	window.addEventListener('popstate', (e) => {
+		// e.stopPropagation();
+		// e.preventDefault();
+		if (!confirm(hint || 'Sure to exit the current page?')) {
+			pushHistoryPage();
+			return;
+		}
+	}, false);
+};
+
+// Use the all available way to keep the page alive with user's agreement.
+const enablePageExitReminder = (hint?: string) => {
+	enablePageCloseReminder(hint);
+	enablePageBackReminder(hint);
+};
+
+export const PageExitUtil = {
+	enablePageExitReminder,
+	enablePageCloseReminder,
+	enablePageBackReminder,
+};
