1613530ccbefe91b12620f8e6a1d513cdc03991f
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-08-04 12:12:18 +0800

    [WebDownloaderUtil] Added util to download text as file for web pages.
    - [ReadableDate] Enriched date util for readability.

--- ReadableDate.ts
+++ ReadableDate.ts
@@ -21,6 +21,16 @@ const formatAsHourMinute = (date?: ParamDate, seperator: string = ':') => {
 	return `${addLeadingZeros(d.getHours(), 2)}${seperator}${addLeadingZeros(d.getMinutes(), 2)}`;
 };
 
+const getReadableMonthDay = (date?: ParamDate, month: string = '月', day: string = '月') => {
+	const d = newDate(date);
+	return `${d.getMonth() + 1}${month}${d.getDate()}${day}`;
+};
+
+const getReadableMonthDayHourMinute = (date?: ParamDate, month: string = '月', day: string = '月') => {
+	const d = newDate(date);
+	return `${d.getMonth() + 1}${month}${d.getDate()}${day} ${addLeadingZeros(d.getHours(), 2)}:${addLeadingZeros(d.getMinutes(), 2)}`;
+};
+
 const getReadableUsedTime = (usedTimeInMillisecond: number): string => {
 	const usedTime = Math.floor(usedTimeInMillisecond / 1000);
 	const min = Math.floor(usedTime / 60);
@@ -31,6 +41,7 @@ export const ReadableDate = {
 	addLeadingZeros,
 
 	formatAsMonthDay, formatAsHourMinute,
+	getReadableMonthDay, getReadableMonthDayHourMinute,
 
 	getReadableUsedTime,
 	getReadableIntervalTime,
